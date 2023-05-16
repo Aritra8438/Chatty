685267e472717d05c40e2d7113c0c6e52915367c
Aritra8438 <aritramajumder8438@gmail.com>
2023-05-16 08:17:57 +0530

    Minor fix

--- Frontend/chatty/src/main.js
+++ Frontend/chatty/src/main.js
@@ -7,13 +7,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBqe-OwI5Y55YsWOKq5s22SXRT5MvtWkpY",
-  authDomain: "chatty-52aa7.firebaseapp.com",
-  projectId: "chatty-52aa7",
-  storageBucket: "chatty-52aa7.appspot.com",
-  messagingSenderId: "532520761164",
-  appId: "1:532520761164:web:47bbb443beed2d58f701e9",
-  measurementId: "G-SRWQVRYGWP",
+  //Get your firebase authentication here
 };
 firebase.initializeApp(firebaseConfig);
 
